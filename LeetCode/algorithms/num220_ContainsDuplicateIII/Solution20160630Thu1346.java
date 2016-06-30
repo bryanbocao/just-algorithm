7783ce67ae8b0a5ff9c056c93c441ae4169770ed
bryanbo-cao <bryan.bo.cao@outlook.com>
2016-06-30 13:48:44 -0600

    Tried num220_ContainsDuplicateIII TimeLimitExceeded

--- LeetCode/algorithms/num220_ContainsDuplicateIII/Solution20160630Thu1346.java
+++ LeetCode/algorithms/num220_ContainsDuplicateIII/Solution20160630Thu1346.java
@@ -1,7 +1,10 @@
 package algorithms.num220_ContainsDuplicateIII;
 
-public class Solution {
+import java.util.Arrays;
+
+public class Solution20160630Thu1346 {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+    	Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
         	for (int j = i + 1; j < nums.length; j++) {
         		if (Math.abs((long)nums[j] - (long)nums[i]) <= t && j - i <= k) return true;
@@ -10,3 +13,4 @@ public class Solution {
         return false;
     }
 }
+//20160630Thu13:46 duration:7m19s36 TimeLimitExceeded @github.com/BryanBo-Cao,hackerrank.com/bryanbocao,linkedin.com/in/bryanbocao
