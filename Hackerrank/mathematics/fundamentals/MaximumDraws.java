c73df65a70cc57a00e604c651512479cce45bb38
bryanbo-cao <bryan.bo.cao@outlook.com>
2016-07-06 16:39:05 -0600

    Solved MaximumDraws

---
+++ Hackerrank/mathematics/fundamentals/MaximumDraws.java
@@ -0,0 +1,19 @@
+package mathematics.fundamentals;
+import java.util.*;
+public class MaximumDraws {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for (int i = 0; i < t; i++) {
+			int n = sc.nextInt();
+			System.out.println(n + 1);
+		}
+		
+		sc.close();
+	}
+
+}
+//https://www.hackerrank.com/challenges/maximum-draws
+//20160706Tud16:37 duration:1m14s70 Accepted @github.com/BryanBo-Cao,hackerrank.com/bryanbocao,linkedin.com/in/bryanbocao
