fe79574a153c60ef78a66001037188ef6330ee96
bryanbo-cao <bryan.bo.cao@outlook.com>
2016-07-21 15:50:15 -0600

    Solved num033_SearchInRotatedSortedArray duration:15m38s52 Accepted(CouldBeImproved)

--- LeetCode/algorithms/num033_SearchInRotatedSortedArray/Test.java
+++ LeetCode/algorithms/num033_SearchInRotatedSortedArray/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		int[] nums = new int[]{20 ,30 ,40 ,50 ,1 ,2 ,3 ,4 ,5, 6, 7 ,8 ,9 ,10};
+		int[] nums = new int[]{20, 30, 40, 50, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		System.out.println(s.search(nums, 10));
 	}
 
