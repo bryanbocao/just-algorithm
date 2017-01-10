f66f33f0ff4b110646673cefe9cbcc9fc9d8c284
BryanBo-Cao <bryan.bo.cao@outlook.com>
2017-01-09 23:03:58 -0700

    Add solution of num095_ValidateBinarySearchTree from https://discuss.leetcode.com/topic/7179/my-simple-java-solution-in-3-lines

---
+++ LintCode/ladders/USGiants/l7_BinaryTree/num095_ValidateBinarySearchTree/Solution_FromOther.java
@@ -0,0 +1,60 @@
+package ladders.USGiants.l6_LinkedList.num095_ValidateBinarySearchTree;
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution_FromOther {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
+//http://www.lintcode.com/en/problem/validate-binary-search-tree/
+/*
+Validate Binary Search Tree
+
+ Description
+ Notes
+ Testcase
+ Judge
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+A single node tree is a BST
+Have you met this question in a real interview? Yes
+Example
+An example:
+
+  2
+ / \
+1   4
+   / \
+  3   5
+The above binary tree is serialized as {2,1,4,#,#,3,5} (in level order).
+
+Tags 
+Divide and Conquer Recursion Binary Search Tree Binary Tree
+*/
+//From https://discuss.leetcode.com/topic/7179/my-simple-java-solution-in-3-lines
+//Author: https://discuss.leetcode.com/user/sruzic
