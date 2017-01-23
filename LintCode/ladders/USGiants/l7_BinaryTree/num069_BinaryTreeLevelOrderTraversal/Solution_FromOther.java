3b6d44c07553d6c164bd99ee5b3cb2a28663de48
BryanBo-Cao <bryan.bo.cao@outlook.com>
2017-01-23 10:23:10 -0700

    Add solution of num069_BinaryTreeLevelOrderTraversal from https://discuss.leetcode.com/topic/7647/java-solution-with-a-queue-used

---
+++ LintCode/ladders/USGiants/l7_BinaryTree/num069_BinaryTreeLevelOrderTraversal/Solution_FromOther.java
@@ -0,0 +1,67 @@
+package ladders.USGiants.l7_BinaryTree.num069_BinaryTreeLevelOrderTraversal;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution_FromOther {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+
+        if(root == null) return wrapList;
+
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i<levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+}
+//http://www.lintcode.com/en/problem/binary-tree-level-order-traversal/
+/*
+Binary Tree Level Order Traversal
+
+ Description
+ Notes
+ Testcase
+ Judge
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+Have you met this question in a real interview? Yes
+Example
+Given binary tree {3,9,20,#,#,15,7},
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+
+return its level order traversal as:
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+Challenge
+Challenge 1: Using only 1 queue to implement it.
+
+Challenge 2: Use DFS algorithm to do it.
+ */
+//From https://discuss.leetcode.com/topic/7647/java-solution-with-a-queue-used
+//Author: https://discuss.leetcode.com/user/soy
---
+++ LintCode/ladders/USGiants/l7_BinaryTree/num073_ConstructBinaryTreeFromPreorderAndInorderTraversal/Solution_FromOther.java
@@ -0,0 +1,53 @@
+package ladders.USGiants.l7_BinaryTree.num073_ConstructBinaryTreeFromPreorderAndInorderTraversal;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution_FromOther {
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(preorder, 0, inorder, 0, inorder.length - 1);
+    }
+
+    public static TreeNode helper(int[] preorder, int preStart, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preorder.length - 1 || inStart > inEnd)
+            return null;
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int target = inStart;
+        while (inorder[target] != preorder[preStart]) target++;
+        root.left = helper(preorder, preStart + 1, inorder, inStart, target - 1);
+        root.right = helper(preorder, preStart + target - inStart + 1, inorder, target + 1, inEnd);
+        return root;
+    }
+}
+//http://www.lintcode.com/en/problem/construct-binary-tree-from-preorder-and-inorder-traversal/
+/*
+Construct Binary Tree from Preorder and Inorder Traversal
+
+ Description
+ Notes
+ Testcase
+ Judge
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+ Notice
+
+You may assume that duplicates do not exist in the tree.
+
+Have you met this question in a real interview? Yes
+Example
+Given in-order [1,2,3] and pre-order [2,1,3], return a tree:
+
+  2
+ / \
+1   3
+Tags
+Binary Tree
+ */
+//From https://discuss.leetcode.com/topic/3695/my-accepted-java-solution/8
+//Author https://discuss.leetcode.com/user/annieqt
