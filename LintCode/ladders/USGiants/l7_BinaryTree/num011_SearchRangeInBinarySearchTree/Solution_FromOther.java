421719b4946fab7f1fbeca1ff7d07411d37f3256
BryanBo-Cao <bryan.bo.cao@outlook.com>
2017-01-26 22:32:45 -0700

    Add solution of num011_SearchRangeInBinarySearchTree from http://www.jiuzhang.com/solutions/search-range-in-binary-search-tree/

---
+++ LintCode/ladders/USGiants/l7_BinaryTree/num011_SearchRangeInBinarySearchTree/Solution_FromOther.java
@@ -0,0 +1,68 @@
+package ladders.USGiants.l7_BinaryTree.num011_SearchRangeInBinarySearchTree;
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution_FromOther {
+    private ArrayList<Integer> results;
+    /**
+     * @param root: The root of the binary search tree.
+     * @param k1 and k2: range k1 to k2.
+     * @return: Return all keys that k1<=key<=k2 in increasing order.
+     */
+    public ArrayList<Integer> searchRange(TreeNode root, int k1, int k2) {
+        results = new ArrayList<Integer>();
+        helper(root, k1, k2);
+        return results;
+    }
+
+    private void helper(TreeNode root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+        if (root.val > k1) {
+            helper(root.left, k1, k2);
+        }
+        if (root.val >= k1 && root.val <= k2) {
+            results.add(root.val);
+        }
+        if (root.val < k2) {
+            helper(root.right, k1, k2);
+        }
+    }
+}
+
+//http://www.lintcode.com/en/problem/search-range-in-binary-search-tree/
+/*
+ Search Range in Binary Search Tree
+
+ Description
+ Notes
+ Testcase
+ Judge
+Given two values k1 and k2 (where k1 < k2) and a root pointer to a Binary Search Tree. Find all the keys of tree in range k1 to k2. i.e. print all x such that k1<=x<=k2 and x is a key of given BST. Return all the keys in ascending order.
+
+Have you met this question in a real interview? Yes
+Example
+If k1 = 10 and k2 = 22, then your function should return [12, 20, 22].
+
+    20
+   /  \
+  8   22
+ / \
+4   12
+Tags
+Binary Search Tree Binary Tree
+Related Problems
+Easy First Position of Target 33 %
+Medium Binary Tree Serialization
+ */
+//From http://www.jiuzhang.com/solutions/search-range-in-binary-search-tree/
