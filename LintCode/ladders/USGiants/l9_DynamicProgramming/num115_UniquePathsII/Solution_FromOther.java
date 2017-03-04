abbd032174d15edd6e894e103eeadca790b8d121
BryanBo-Cao <bryan.bo.cao@outlook.com>
2017-03-03 23:21:20 -0700

    Add solution of num115_UniquePathsII from https://discuss.leetcode.com/topic/10974/short-java-solution/2

---
+++ LintCode/ladders/USGiants/l9_DynamicProgramming/num115_UniquePathsII/Solution_FromOther.java
@@ -0,0 +1,53 @@
+package ladders.USGiants.l9_DynamicProgramming.num115_UniquePathsII;
+
+public class Solution_FromOther {
+    /**
+     * @param obstacleGrid: A list of lists of integers
+     * @return: An integer
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int width = obstacleGrid[0].length;
+        int[] dp = new int[width];
+        dp[0] = 1;
+        for (int[] row : obstacleGrid) {
+            for (int j = 0; j < width; j++) {
+                if (row[j] == 1) dp[j] = 0;
+                else if (j > 0) dp[j] += dp[j - 1];
+            }
+        }
+        return dp[width - 1];
+    }
+}
+//
+/*
+Unique Paths II
+
+ Description
+ Notes
+ Testcase
+ Judge
+Follow up for "Unique Paths":
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+ Notice
+
+m and n will be at most 100.
+
+Have you met this question in a real interview? Yes
+Example
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+*/
+//From https://discuss.leetcode.com/topic/10974/short-java-solution/2
+//Author https://discuss.leetcode.com/user/tusizi
+//PracticedOn20170303Fri 5Times
