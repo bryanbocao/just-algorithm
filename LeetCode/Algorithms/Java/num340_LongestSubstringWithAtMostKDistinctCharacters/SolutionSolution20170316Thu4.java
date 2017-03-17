7dca7cb59b963510347edf9d28e876c68e23c24e
BryanBo-Cao <bryan.bo.cao@outlook.com>
2017-03-16 23:16:51 -0600

    Solve num340_LongestSubstringWithAtMostKDistinctCharacters

--- LeetCode/Algorithms/Java/num340_LongestSubstringWithAtMostKDistinctCharacters/SolutionSolution20170316Thu4.java
+++ LeetCode/Algorithms/Java/num340_LongestSubstringWithAtMostKDistinctCharacters/SolutionSolution20170316Thu4.java
@@ -1,8 +1,9 @@
 package com.num340_LongestSubstringWithAtMostKDistinctCharacters;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Solution {
+public class SolutionSolution20170316Thu4 {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
         int leftIndex = 0, res = 0;
@@ -10,11 +11,27 @@ public class Solution {
         for (int rightIndex = 0; rightIndex < s.length(); rightIndex++) {
             if (map.get(cs[rightIndex]) == null) map.put(cs[rightIndex], 1);
             else map.put(cs[rightIndex], map.get(cs[rightIndex]) + 1);
+            System.out.println();
+            System.out.println("leftIndex: " + leftIndex + " rightIndex: " + rightIndex);
+            System.out.println(cs[rightIndex]);
+            System.out.println("cs[leftIndex]: " + cs[leftIndex] + "map.get(cs[leftIndex]): " + map.get(cs[leftIndex]));
             if (map.size() > k) {
-                while (leftIndex++ < cs.length && map.get(cs[leftIndex - 1]) != null && map.get(cs[leftIndex - 1]) - 1 > 0)
+                System.out.println();
+                System.out.println(" > k");
+                System.out.println("> k leftIndex: " + leftIndex + " rightIndex: " + rightIndex);
+                System.out.println(cs[rightIndex]);
+                System.out.println("> k cs[leftIndex]: " + cs[leftIndex] + "map.get(cs[leftIndex]): " + map.get(cs[leftIndex]));
+
+                while (leftIndex++ < cs.length && map.get(cs[leftIndex - 1]) != null && map.get(cs[leftIndex - 1]) - 1 > 0) {
                     map.put(cs[leftIndex - 1], map.get(cs[leftIndex - 1]) - 1);
+                    System.out.println(leftIndex - 1);
+                    System.out.println("while leftIndex - 1: " + (leftIndex - 1) + " rightIndex: " + rightIndex);
+                }
                 map.remove(cs[leftIndex - 1]);
+                System.out.println("line 24");
             }
+            System.out.println("rightIndex - leftIndex + 1");
+            System.out.println(rightIndex - leftIndex + 1);
             res = Math.max(res, rightIndex - leftIndex + 1);
         }
         return res;
@@ -36,4 +53,29 @@ T is "ece" which its length is 3.
  */
 //Inspired By https://discuss.leetcode.com/topic/41671/15-lines-java-solution-using-slide-window
 //Author: https://discuss.leetcode.com/user/jiangbowei2010
-//SolvedOn20170316Thu
+//TriedOn20170316Thu
+/*
+Input:
+"bacc"
+2
+Output:
+2
+Expected:
+3
+
+Input:
+"a"
+1
+Output:
+0
+Expected:
+1
+
+Input:
+"eceba"
+2
+Output:
+4
+Expected:
+3
+ */
